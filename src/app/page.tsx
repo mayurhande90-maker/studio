13b9a075f7e9535feea27ed0da048f7a44e5565d
Mayur Hande <mayurhande90@gmail.com>
2025-10-11 12:50:07 +0000

    remove the see the magic in action section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import { Hero } from '@/components/sections/hero';
 import { Features } from '@/components/sections/features';
 import { Reviews } from '@/components/sections/reviews';
 import { Pricing } from '@/components/sections/pricing';
-import { Showcase } from '@/components/sections/showcase';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
-      <Showcase />
       <Reviews />
       <Pricing />
     </>
