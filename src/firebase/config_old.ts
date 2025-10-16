e4ae0d9c2d53360bfb90220e4bdd17c2027c0a16
mayurhande90-maker <mayurhande90@gmail.com>
2025-10-16 16:42:18 +0530

    Delete src/firebase directory
    Fixed Firebase imports and removed legacy src/firebase folder

--- src/firebase/config_old.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5028520146-48db9",
-  "appId": "1:321672599979:web:309245f44033b90e795aee",
-  "apiKey": "AIzaSyAlQlbEyetuldbl5q8fc7EDlGLX56hP6fo",
-  "authDomain": "studio-5028520146-48db9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "321672599979"
-};
